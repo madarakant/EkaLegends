fa70103624ddc84a885d8e2a825912f400774c36
madarakant <madarakant@gmail.com>
2025-04-10 12:12:56 +0300

    Implemented Main Character

---
+++ EkaLegendsNew/Assets/Scripts/PlayerMouseControl.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMouseControl : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private float moveSpeed = 10;
+    
+    private CharacterController characterController;
+    private Vector3 targetPosition;
+    private Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        targetPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distToTarget = Vector3.Distance(transform.position, targetPosition);
+        if (distToTarget > 1f)
+        {
+            Vector3 targetDirection = Vector3.Normalize(targetPosition - transform.position);
+            characterController.Move(targetDirection * moveSpeed * Time.deltaTime);
+            transform.LookAt(targetPosition);
+            animator.SetBool("running", true);
+        }
+        else
+        {
+            animator.SetBool("running", false);
+        }
+        
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 500, layerMask))
+            {
+                Debug.Log("hit: " + hit.collider.name);
+                targetPosition = hit.point;
+            }
+        }
+    }
+}
